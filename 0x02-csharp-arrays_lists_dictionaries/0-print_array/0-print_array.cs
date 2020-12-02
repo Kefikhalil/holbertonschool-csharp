2bdd5e1e39103730efefdc711f4b8ead92189440
Kefikhalil <naroqt@gmail.com>
2020-12-02 12:24:34 -0800

    updated

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size can't be negative");
+            return null;
+        }
+        int[] array1 = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array1[i] = i;
+            if (i != 0)
+                Console.Write(" ");
+            Console.Write(i);
+        }
+        Console.WriteLine("");
+        return array1;
+    }
+}
