c2d762e848d6336bfa7688824f24ca31782e9b27
Kefikhalil <naroqt@gmail.com>
2021-03-03 17:39:52 -0800

    updated

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Enemies
+{
+    ///<summary> Represents a zombie class  ///</summary>
+    public class Zombie
+    {
+        ///<summary> Represent the zombie health </summary>
+	public int health;
+	///<summary> Represent the zombie constructor </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+            ///<summary> New instance of a zombie </summary>
+        public Zombie (int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+	        ///<summary> Return the zombie current health </summary>
+        public int GetHealth()
+        {
+            return (health);
+        }
+    }
+}
