25b03d6502614369ddf6da64d839643e83575d81
Kefikhalil <naroqt@gmail.com>
2021-03-03 17:27:15 -0800

    updated

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -2,9 +2,12 @@ using System;
 
 namespace Enemies
 {
+    ///<summary> Represents a zombie class  ///</summary>
     public class Zombie
     {
-        public int health;
+        ///<summary> Represent the zombie health </summary>
+	public int health;
+	///<summary> Represent the zombie constructor </summary>
         public Zombie()
         {
             health = 0;
