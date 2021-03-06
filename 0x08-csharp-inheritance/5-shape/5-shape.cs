a96940447f9d112a47adaa04abc0a278c4eb01d9
Kefikhalil <naroqt@gmail.com>
2021-03-05 16:27:14 -0800

    updated

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+using System;
+
+///<summary> Represent a shape </summary>
+    class Shape
+    {
+	///<summary> area of a shape </summary>    
+        public virtual int Area()
+        {
+            throw new NotImplementedException("Area() is not implemented");
+        }
+    }
