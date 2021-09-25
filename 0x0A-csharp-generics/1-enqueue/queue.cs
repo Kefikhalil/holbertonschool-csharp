e1b05c14b2c180d056a1167a66a8fff5bd50ed73
Kefikhalil <naroqt@gmail.com>
2021-09-24 18:58:39 -0700

    updated

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,49 @@
+using System;
+///<summary>Queue Class </summary>
+class Queue<T>
+{
+	public int count;
+	public Node head;
+	public Node tail;
+
+///<summary>Checks if its a queue type </summary>
+
+	public Type CheckType()
+	{
+		return typeof(T);
+	}
+	public class Node
+	{
+		private T value;
+		public Node next = null;
+
+		public Node(T string)
+		{
+			value = string;
+		}
+	}
+	///<summary>ENqueue</summary>
+	public void Enqueue(T value)
+	{
+		Node newnode = new Node(value);
+		if (head == null)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail.next = newnode;
+			tail = newnode;
+		}
+		count++;
+	}
+
+	///<summary> count </summary>
+	public int Count()
+	{
+		return count;
+	}
+
+
+}
