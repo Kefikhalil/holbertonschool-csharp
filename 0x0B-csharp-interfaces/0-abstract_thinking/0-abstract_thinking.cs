d70184cca373ec864ae4fbd55fa78afb977fb521
Kefikhalil <naroqt@gmail.com>
2021-09-27 18:19:16 -0700

    updated

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -4,8 +4,9 @@ using System;
 ///<summary>Base class.</summary>
 public abstract class Base
 {
-public string name;
+public string name {get; set;}
     public override string ToString()
     {
         return ($"{this.name} is a {this.GetType()}");
+    }
 }
