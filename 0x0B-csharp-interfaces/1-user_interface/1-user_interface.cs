ac6e36b6e3a39554bfc97bc90b2445c9826b3c08
Kefikhalil <naroqt@gmail.com>
2021-09-28 21:36:19 -0700

    updated

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -8,40 +8,37 @@ abstract class Base{
         return ($"{name} is a {this.GetType()}");
     }
 }
-/// <summary>Interactive interface</summary>
-public interface IInteractive{
-    /// <summary>interactions </summary>
+interface IInteractive
+{
     void Interact();
 }
-/// <summary>IBreakable</summary>
-public interface IBreakable{
-    /// <summary>Used for item durability</summary>
-    int durability {get; set;}
-    /// <summary>Used to break an item</summary>
+
+interface IBreakable
+{
+    int durability { get; set; }
     void Break();
 }
-/// <summary>ICollectable interface</summary>
-public interface ICollectable{
-    /// <summary>Used to check if collected or not</summary>
-    bool isCollected {get; set;}
-    /// <summary>Used to collect something</summary>
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
     void Collect();
 }
-/// <summary>Used for test purpose</summary>
-public class TestObject : Base, IInteractive, IBreakable, ICollectable{
 
-    /// <summary>Used for item durability</summary>
-    public int durability {get; set;}
-    /// <summary>Used for collection</summary>
-    public bool isCollected {get; set;}
-    /// <summary>Used for interaction</summary>
-    public void Interact(){
-    }
-    /// <summary>Used to break item</summary>
-    public void Break(){
-    }
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+    public void Interact()
+    {
 
-    /// <summary>Used to collect something</summary>
-    public void Collect(){
+    }
+    public void Break()
+    {
+
+    }
+    public void Collect()
+    {
+
     }
 }
