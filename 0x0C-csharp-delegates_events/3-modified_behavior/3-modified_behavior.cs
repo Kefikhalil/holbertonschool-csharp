f09b19fa376d7cc22ddbeef9cbee709f5753dad5
Kefikhalil <naroqt@gmail.com>
2021-10-02 20:33:35 -0700

    updated

--- 0x0C-csharp-delegates_events/3-modified_behavior/3-modified_behavior.cs
+++ 0x0C-csharp-delegates_events/3-modified_behavior/3-modified_behavior.cs
@@ -71,11 +71,6 @@ public class Player
 }
     public float ApplyModifier(float baseValue, Modifier modifier)
     {
-        if (modifier == Modifier.Weak)
-            return (float)(baseValue / 2);
-        else if (modifier == Modifier.Base)
-            return (baseValue);
-        else
-            return (float)(baseValue * 1.5);
+        return (baseValue * ((float)modifier / 2f));
 }
 }
